a8ee4093298c29588d27dc943d49b8228d408493
kuba <jakubpapuda27@gmail.com>
2024-04-13 12:36:07 +0200

    Added gitignore

--- MockDb.cs
+++ MockDb.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace API;
 
 public interface IMockDb
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 using API;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
